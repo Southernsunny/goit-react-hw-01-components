526b682ac66791e586737a5f81e631ed3800bf57
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-05-18 16:54:49 +0300

    add completed task

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li className="item" key={id}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
